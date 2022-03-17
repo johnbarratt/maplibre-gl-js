96166163568cc83fb8e8e67af2d80894fcc9c646
John Barratt <djon00@gmail.com>
2022-03-17 00:23:15 +0000

    Fix missing parameter that causes jitter when zooming 3d objects

--- src/geo/transform.ts
+++ src/geo/transform.ts
@@ -727,7 +727,7 @@ class Transform {
         this.mercatorMatrix = mat4.scale([] as any, m, vec3.fromValues(this.worldSize, this.worldSize, this.worldSize));
 
         // scale vertically to meters per pixel (inverse of ground resolution):
-        mat4.scale(m, m, vec3.fromValues(1, 1, mercatorZfromAltitude(1, this.center.lat) * this.worldSize));
+        mat4.scale(m, m, vec3.fromValues(1, 1, mercatorZfromAltitude(1, this.center.lat) * this.worldSize),1));
 
         this.projMatrix = m;
         this.invProjMatrix = mat4.invert([] as any, this.projMatrix);
